345d1f8ba8f8788a5d0b9630006345fb572cd100
Yuheng <appleplayer8@gmail.com>
2020-07-07 15:17:37 -0700

    slight ui adjustment

--- lib/camera_screen.dart
+++ lib/camera_screen.dart
@@ -1,33 +1,3 @@
-/*
- * Copyright (c) 2019 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
- * distribute, sublicense, create a derivative work, and/or sell copies of the
- * Software in any work that is designed, intended, or marketed for pedagogical or
- * instructional purposes related to programming, coding, application development,
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works,
- * or sale is expressly withheld.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
@@ -100,8 +70,10 @@ class _CameraScreenState extends State {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Click To Share'),
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.lightBlue,
+        centerTitle: true,
+        elevation: 5,
+        title: const Text('Pathomatic'),
       ),
       body: Container(
         child: SafeArea(
@@ -112,16 +84,18 @@ class _CameraScreenState extends State {
                 flex: 2,
                 child: _cameraPreviewWidget(context),
               ),
-              SizedBox(height: 10.0),
+              SizedBox(
+                height: 10.0,
+              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   _cameraTogglesRowWidget(),
                   _captureControlRowWidget(context),
                   Spacer()
                 ],
               ),
-              SizedBox(height: 20.0)
+              SizedBox(height: 10.0)
             ],
           ),
         ),
@@ -161,8 +135,8 @@ class _CameraScreenState extends State {
           mainAxisSize: MainAxisSize.max,
           children: [
             FloatingActionButton(
-                child: Icon(Icons.camera),
-                backgroundColor: Colors.blueGrey,
+                child: Icon(Icons.camera_alt),
+                backgroundColor: Colors.lightBlue,
                 onPressed: () {
                   _onCapturePressed(context);
                 })
