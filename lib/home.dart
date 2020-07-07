d35b829ed74bc3eba74ad09443b43e97e9f6f39f
umar.ali <umaru.alia@gmail.com>
2020-07-06 17:29:42 -0700

    ML text update (no model update yet)

--- lib/home.dart
+++ lib/home.dart
@@ -81,20 +81,24 @@ class _HomePageState extends State<HomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   RaisedButton(
-                    child: const Text(ssd),
+                    child: const Text('4x Magnification'),
                     onPressed: () => onSelect(ssd),
                   ),
                   RaisedButton(
-                    child: const Text(yolo),
+                    child: const Text('10x Magnification'),
                     onPressed: () => onSelect(yolo),
                   ),
                   RaisedButton(
-                    child: const Text(mobilenet),
+                    child: const Text('25x Magnification'),
                     onPressed: () => onSelect(mobilenet),
                   ),
                   RaisedButton(
-                    child: const Text(posenet),
+                    child: const Text('40x Magnification'),
                     onPressed: () => onSelect(posenet),
+                  ),
+                  RaisedButton(
+                    child: const Text('63x Magnification'),
+                    onPressed: () => onSelect(mobilenet),
                   ),
                 ],
               ),
