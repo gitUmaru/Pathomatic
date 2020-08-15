1b86cb8eda5c1e667ace11571b2d00d60d71ec1b
umar.ali <umaru.alia@gmail.com>
2020-08-14 22:23:16 -0700

    global var is saved + camea returns patientID

--- lib/front_end/getPatientIdentifier.dart
+++ lib/front_end/getPatientIdentifier.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../back_end/globals.dart' as globals;
 
 class PatientIdentifier extends StatefulWidget {
     PatientIdentifier({
@@ -13,7 +14,6 @@ class PatientIdentifier extends StatefulWidget {
 }
 
 
-String patientIdentifier;
 class _PatientIdentifierState extends State<PatientIdentifier> {
   final myController = TextEditingController();
 
@@ -30,28 +30,29 @@ class _PatientIdentifierState extends State<PatientIdentifier> {
         title: Text('Unique Patient Identifier'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-        children: <Widget>[
-          TextField(
-            controller: myController,
-          ),
-          Text(
-            "Please work...",
-          )
-        ])
-      ),
-      floatingActionButton: FloatingActionButton(
+          padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Please enter a unique patient identifier. Note that every new patient will require you to restart the app and enter a new patient identifier.',
+            ),
+            TextField(
+              controller: myController,
+            ),
+          ],
+        ),
+        ),
+        floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(context).pushNamed(
             '/dashboard',
-            arguments: myController.text,
+            arguments: globals.patientIdentifier = myController.text,
           );
         },
-        tooltip: 'Show me the value!',
+        tooltip: 'Go next',
         child: Icon(Icons.arrow_forward_ios),
       ),
     );
-    patientIdentifier = myController.text;
   }
 }
