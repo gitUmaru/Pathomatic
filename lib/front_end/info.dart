6b5274f55bbe24d5b12ffc6b7bc182695fe134c0
Yuheng <appleplayer8@gmail.com>
2020-08-19 12:27:30 -0700

    Added form validation

--- lib/front_end/info.dart
+++ lib/front_end/info.dart
@@ -14,6 +14,8 @@ class PatientIdentifier extends StatefulWidget {
 }
 
 class _PatientIdentifierState extends State<PatientIdentifier> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void dispose() {
     globals.patientID.dispose();
@@ -34,62 +36,86 @@ class _PatientIdentifierState extends State<PatientIdentifier> {
         automaticallyImplyLeading: false,
         title: Text('Identification'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
-          child: Container(
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                    controller: globals.patientID,
-                    decoration: new InputDecoration(
-                        labelText: 'Please enter a unique patient identifier',
-                        fillColor: Colors.white,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(25.0),
-                          borderSide: new BorderSide(),
-                        ))),
-                Text(""),
-                Text(""),
-                TextFormField(
-                    controller: globals.name,
-                    decoration: new InputDecoration(
-                        labelText: 'Please enter your name',
-                        fillColor: Colors.white,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(25.0),
-                          borderSide: new BorderSide(),
-                        ))),
-                Text(""),
-                Text(""),
-                TextFormField(
-                    controller: globals.email,
-                    decoration: new InputDecoration(
-                        labelText: 'Please enter your email',
-                        fillColor: Colors.white,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(25.0),
-                          borderSide: new BorderSide(),
-                        ))),
-                Text(""),
-                Text(""),
-                TextFormField(
-                    controller: globals.hospital,
-                    decoration: new InputDecoration(
-                        labelText: 'Please enter your hospital',
-                        fillColor: Colors.white,
-                        border: new OutlineInputBorder(
-                          borderRadius: new BorderRadius.circular(25.0),
-                          borderSide: new BorderSide(),
-                        ))),
-                Text(""),
-                Text(""),
-                Text(
-                  "Note that for every new patient you will be required to restart the app and enter a new patient identifier.",
-                  style: TextStyle(height: 1.5, fontSize: 14.0),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20, 40, 20, 0),
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                      controller: globals.patientID,
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return 'Patient ID cannot be empty';
+                        }
+                        return null;
+                      },
+                      decoration: new InputDecoration(
+                          labelText: 'Please enter a unique patient identifier',
+                          fillColor: Colors.white,
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: new BorderSide(),
+                          ))),
+                  Text(""),
+                  TextFormField(
+                      controller: globals.name,
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return 'Name cannot be empty';
+                        }
+                        return null;
+                      },
+                      decoration: new InputDecoration(
+                          labelText: 'Please enter your name',
+                          fillColor: Colors.white,
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: new BorderSide(),
+                          ))),
+                  Text(""),
+                  TextFormField(
+                      controller: globals.email,
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return 'Email cannot be empty';
+                        }
+                        return null;
+                      },
+                      decoration: new InputDecoration(
+                          labelText: 'Please enter your email',
+                          fillColor: Colors.white,
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: new BorderSide(),
+                          ))),
+                  Text(""),
+                  TextFormField(
+                      controller: globals.hospital,
+                      validator: (text) {
+                        if (text == null || text.isEmpty) {
+                          return 'Hospital cannot be empty';
+                        }
+                        return null;
+                      },
+                      decoration: new InputDecoration(
+                          labelText: 'Please enter your hospital',
+                          fillColor: Colors.white,
+                          border: new OutlineInputBorder(
+                            borderRadius: new BorderRadius.circular(25.0),
+                            borderSide: new BorderSide(),
+                          ))),
+                  Text(""),
+                  Text(""),
+                  Text(
+                    "Note that for every new patient you will be required to restart the app and enter a new patient identifier.",
+                    style: TextStyle(height: 1.5, fontSize: 14.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -98,10 +124,12 @@ class _PatientIdentifierState extends State<PatientIdentifier> {
         elevation: 8.0,
         backgroundColor: Colors.lightBlue,
         onPressed: () {
-          Navigator.of(context).pushNamed(
-            '/dashboard',
-            arguments: globals.name.text,
-          );
+          if (_formKey.currentState.validate()) {
+            Navigator.of(context).pushNamed(
+              '/dashboard',
+              arguments: globals.name.text,
+            );
+          }
         },
         tooltip: 'Go next',
         child: Icon(Icons.arrow_forward_ios),
